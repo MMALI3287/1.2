9257fbb6d8aac24a290bb97ee23f501985c66240
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2022-05-08 03:46:03 +0600

    8.5.22 v2

--- CGPA_Calc.java
+++ CGPA_Calc.java
@@ -4,23 +4,23 @@ public class CGPA_Calc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input obtained GPA in");
-        System.out.print("CSE1101 : ");
+        System.out.print("CSE1203 : ");
         float E1 = sc.nextFloat();
-        System.out.print("CSE1102 : ");
+        System.out.print("EEE1216 : ");
         float E2 = sc.nextFloat();
-        System.out.print("CSE1108 : ");
+        System.out.print("CSE1206 : ");
         float E3 = sc.nextFloat();
-        System.out.print("PHY1115 : ");
+        System.out.print("EEE1215 : ");
         float P1 = sc.nextFloat();
-        System.out.print("CHEM115 : ");
+        System.out.print("ME1215 : ");
         float C1 = sc.nextFloat();
-        System.out.print("MATH1115 : ");
+        System.out.print("MATH1211 : ");
         float M1 = sc.nextFloat();
-        System.out.print("PHY1116 : ");
+        System.out.print("ME1216 : ");
         float P2 = sc.nextFloat();
-        System.out.print("HUM1107 : ");
+        System.out.print("CSE1205 : ");
         float H1 = sc.nextFloat();
-        System.out.print("HUM1108 : ");
+        System.out.print("CSE1200 : ");
         float H2 = sc.nextFloat();
         System.out.println("Your CGPA is : "
                 + ((((E1 + P1 + C1 + M1 + H1) * 3) + ((E2 + E3 + H2) * 1.5) + (P2 * 0.75)) / (float) 20.25));
